929c0c0d9d0efc38108470208d98ee49ed49ea92
Kohym <marcounek@gmail.com>
2024-04-26 09:08:26 +0200

    updt

--- Dungodon/Scripts/knight.cs
+++ Dungodon/Scripts/knight.cs
@@ -3,19 +3,23 @@ using System;
 
 public partial class knight : CharacterBody2D
 {
-    [Export] 
-    private int speed = 50; 
-    private Vector2 currentVelocity; 
-    public override void _PhysicsProcess (double delta) 
-        {
-        base._PhysicsProcess (delta); 
-        handleInput(); 
-        Velocity = currentVelocity; 
-        MoveAndSlide(); 
-        } 
-        private void handleInput() 
-        { 
-            currentVelocity = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down"); 
-            currentVelocity *= speed; 
-        }
+	[Export] 
+	private int speed = 250; 
+	private Vector2 currentVelocity;
+	private AnimatedSprite2D knightanim;
+	public override void _PhysicsProcess (double delta) 
+	{
+		LookAt(GetGlobalMousePosition());
+		base._PhysicsProcess (delta); 
+		handleInput(); 
+		Velocity = currentVelocity; 
+		MoveAndSlide(); 
+	} 
+	private void handleInput() 
+	{ 
+			currentVelocity = Input.GetVector("left", "right", "up", "down"); 
+			currentVelocity *= speed; 
+	}
+
+
 }
