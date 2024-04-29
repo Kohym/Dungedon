aafdfe64edd316368a1c894ef465e0d5f5101851
Kohym <marcounek@gmail.com>
2024-04-29 12:02:24 +0200

    door update

--- Dungodon/Scripts/door.gd
+++ Dungodon/Scripts/door.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
-@export var iflocked = "Door is Locked"
+@export var isopen = false
 @export var islocked = false
-@onready var door_sprite = $door_sprite
-@onready var door_collbox = $door_collbox
+@export var iflocked = "Door is Locked"
+func _ready():
+	if (isopen == true):
+		$door_sprite.play("open")
+		$door_collbox.set_deferred("disabled", true)
+
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player_body"):
 		if (islocked == false):
-			door_sprite.play("open")
-			door_collbox.set_deferred("disabled", true)
+			$door_sprite.play("open")
+			$door_collbox.set_deferred("disabled", true)
+			await get_tree().create_timer(2.0).timeout
+			$door_sprite.play("closed")
+			$door_collbox.set_deferred("disabled", false)
 		if (islocked == true):
 			print("locked")
 			$door_label.text = str(iflocked)
