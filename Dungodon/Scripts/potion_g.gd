d374104197775b77fcf9baef19ecad5d0b9ebe3d
Kohym <marcounek@gmail.com>
2024-05-17 22:56:50 +0200

    ptn

--- Dungodon/Scripts/potion_g.gd
+++ Dungodon/Scripts/potion_g.gd
@@ -1,6 +1,30 @@
 extends Area2D
 
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	var randidk = rng.randi_range(0,1)
+	if (randidk ==1):
+		$potionG_sprite.flip_h = true
+	else:
+		$potionG_sprite.flip_h = false
 
 func _on_body_entered(body):
 	if body.is_in_group("player_body"):
-		queue_free()
+		set_deferred("monitoring", false)
+		set_deferred("monitorable", false)
+		$potionG_collbox.disabled = true
+		$potionG_sprite.rotate(rng.randi_range(0,360))
+		var randint = rng.randi_range(1,3)
+		print(randint)
+		if (randint == 1):
+			$potionG_sprite.play("broken1")
+		elif (randint == 2):
+			$potionG_sprite.play("broken2")
+		elif (randint == 3):
+			$potionG_sprite.play("broken3")
+		var randbool = rng.randi_range(0,3)
+		if (randbool ==0):
+			$potionG_sprite.flip_h = true
+		elif (randbool ==2):
+			$potionG_sprite.flip_v = true
