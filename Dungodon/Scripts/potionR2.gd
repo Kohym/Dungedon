ec82fadde9ba7da39b5a7d5966d77b336dab588e
Josifek <100674201+VelkejKluk@users.noreply.github.com>
2024-06-03 10:59:04 +0200

    hello

---
+++ Dungodon/Scripts/potionR2.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	var randidk = rng.randi_range(0,1)
+	if (randidk ==1):
+		$potionR2_sprite.flip_h = true
+	else:
+		$potionR2_sprite.flip_h = false
+
+func _on_body_entered(body):
+	if body.is_in_group("player_body"):
+		set_deferred("monitoring", false)
+		set_deferred("monitorable", false)
+		$potionR2_collbox.disabled = true
+		$potionR2_sprite.rotate(rng.randi_range(0,360))
+		var randint = rng.randi_range(1,3)
+		print(randint)
+		if (randint == 1):
+			$potionR2_sprite.play("broken1")
+		elif (randint == 2):
+			$potionR2_sprite.play("broken2")
+		elif (randint == 3):
+			$potionR2_sprite.play("broken3")
+		var randbool = rng.randi_range(0,3)
+		if (randbool ==0):
+			$potionR2_sprite.flip_h = true
+		elif (randbool ==2):
+			$potionR2_sprite.flip_v = true
+
