8de245b9ff7c819f8426072c0077dee98194cb2a
Kohym <marcounek@gmail.com>
2024-06-08 00:28:03 +0200

    JKL

--- Dungodon/Scripts/main_menu.gd
+++ Dungodon/Scripts/main_menu.gd
@@ -1,19 +1,12 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_play_button_pressed():
+func _on_levels_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/levels/game_level.tscn")
-
-
+	
+func _on_options_button_pressed():
+	pass # Replace with function body.
+	
 func _on_exit_button_pressed():
 	get_tree().quit()
+
+
