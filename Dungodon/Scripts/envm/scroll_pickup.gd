f45bcf900e82cfc5c4b961228d9d5d9a165a295c
Kohym <marcounek@gmail.com>
2024-06-10 10:59:30 +0200

    dfgfsdfvfsdygh

--- Dungodon/Scripts/envm/scroll_pickup.gd
+++ Dungodon/Scripts/envm/scroll_pickup.gd
@@ -1,8 +1,12 @@
 extends Area2D
 @export var whattoopen: Control
 var debug = true
+var firs_time_open = false
 func _on_body_entered(body):
 	if body.is_in_group("player_body") and debug == true:
+		if firs_time_open == false:
+			firs_time_open == true
+			$scroll_texture.play("used")
 		debug = false
 		whattoopen.visible = true
 
